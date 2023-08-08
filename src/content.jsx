858ea643f3600fe65ba86b70182a8e5e4709afb0
Анастасия <nastasia6819@gmail.com>
2023-08-08 14:24:02 +0300

    Deletes marker

--- src/content.jsx
+++ src/content.jsx
@@ -3,35 +3,12 @@ import MapGL, { Source, Layer, FullscreenControl, NavigationControl } from '@urb
 import 'mapbox-gl/dist/mapbox-gl.css';
 import {ACCESS_TOKEN} from './service'
 
+
 const Content = ({data, onClick}) => {
   const [viewport, setViewport] = useState({
     latitude: 59.944548,
     longitude: 30.304617
   })
-
-  // const [position, setPosition] = useState({
-  //   longitude: 0,
-  //   latitude: 0
-  // });
-
-  // const style = {
-  // padding: '10px',
-  // color: '#fff',
-  // cursor: 'pointer',
-  // background: '#1978c8',
-  // borderRadius: '10px'
-  // };
-  
-  // const onClick = (event) => {
-  //   setPosition({ 
-  //     longitude: event.lngLat.lng, 
-  //     latitude: event.lngLat.lat 
-  //   });
-  // };
-
-  //  const onMarkerClick = (event) => {
-  //     event.stopPropagation();
-  //  };
 
   return (
     
@@ -56,13 +33,6 @@ const Content = ({data, onClick}) => {
           'fill-opacity': 0.8
         }}
       /></> : null}
-      {/* <Marker
-        latitude={position.latitude}
-        longitude={position.longitude}
-        onClick={onMarkerClick}
-      >
-        <div style={style}></div>
-      </Marker>  */}
       <NavigationControl showCompass showZoom position='top-right' />
       <FullscreenControl position='top-right' />
     </MapGL>
